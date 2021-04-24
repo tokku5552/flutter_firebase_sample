368fdea7384394fdcd1784cc886e1eb2a6648ae5
tokku5552 <physics.0121@gmail.com>
2021-04-25 00:00:32 +0900

    fix:error

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({required this.title});
 
   final String title;
 
